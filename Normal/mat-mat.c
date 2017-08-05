67dc4d327c0b6f6f60be954e13ffbbf070e71149
kngwyu <yuji.kngw.80s.revive@gmail.com>
2017-08-05 23:12:55 +0900

    modified normal

--- Normal/mat-mat.c
+++ Normal/mat-mat.c
@@ -5,6 +5,7 @@
 
 #define  N        576
 #define  NPROCS   288
+#define  (N / NPROCS)
 
 #define  DEBUG  1
 #define  EPS    1.0e-18
@@ -14,41 +15,56 @@
 #define  COPY(a, b, n) do{int i;for(i=0;i<n;++i){b[i]=a[i];}}while(0);
 
 int myid, numprocs;
-void MyMatMat(double* c, double* a, double* b, int n);
+void MyMatMat(double* c, double* a, double* b);
 int main(int argc, char* argv[]) {
     double  t0, t1, t2, t_w;
     double  dc_inv, d_mflops;
     int     ierr;
     int     i, j;      
     int     iflag, iflag_t;
+    if (N % NPROCS != 0) {
+        puts("N % NPROCS != 0");
+        exit(0);
+    }
     ierr = MPI_Init(&argc, &argv);
     ierr = MPI_Comm_rank(MPI_COMM_WORLD, &myid);
     ierr = MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
-    int istart = (N / NPROCS) * myid;
-    int iend = MIN(istart + N / NPROCS, N);
-    int block_len = iend - istart;
-    double* a = (double*)malloc(sizeof(double) * N * block_len);
-    double* b = (double*)malloc(sizeof(double) * N * block_len);
-    double* c = (double*)malloc(sizeof(double) * N * block_len);
+    static double a[BLOCK_LEN][N];
+    static double b[N][BLOCK_LEN];
+    static double c[BLOCK_LEN][N];
     /* matrix generation --------------------------*/
     if (DEBUG == 1) {
-        FILL(a, N * block_len, 1.0);
-        FILL(b, N * block_len, 1.0);
-        FILL(c, N * block_len, 0.0);
+        for (i = 0; i < BLOCK_LEN; ++i) {
+            for (j = 0; j < N; ++j) {
+                a[i][j] = 1.0;
+                c[i][j] = 0.0;
+            }
+        }
+        for (i = 0; i < N; ++i) {
+            for (j = 0; j < BLOCK_LEN; ++j) {
+                b[i][j] = 1.0;
+            }
+        }
     } else {
         srand(myid);
         dc_inv = 1.0/(double)RAND_MAX;
-        for (i = 0; i < N * block_len; ++i) {
-            a[i] = rand() * dc_inv;
-            b[i] = rand() * dc_inv;
-            c[i] = 0.0;
+        for (i = 0; i < BLOCK_LEN; ++i) {
+            for (j = 0; j < N; ++j) {
+                a[i][j] = rand() * dc_inv;
+                c[i][j] = 0.0;
+            }
+        }
+        for (i = 0; i < N; ++i) {
+            for (j = 0; j < BLOCK_LEN; ++j) {
+                b[i][j] = rand() * dc_inv();
+            }
         }
     } /* end of matrix generation --------------------------*/
 
     /* Start of mat-vec routine ----------------------------*/
     ierr = MPI_Barrier(MPI_COMM_WORLD);
     t1 = MPI_Wtime();
-    MyMatMat(c, a, b, N);
+    MyMatMat(c, a, b);
     ierr = MPI_Barrier(MPI_COMM_WORLD);
     t2 = MPI_Wtime();
     t0 =  t2 - t1; 
@@ -68,10 +84,10 @@ int main(int argc, char* argv[]) {
     if (DEBUG == 1) {
         /* Verification routine ----------------- */
         iflag = 0;
-        for(i = 0; i < N * block_len; ++i) {
+        for(i = 0; i < BLOCK_LEN; ++i) {
             for (j = 0; j < N; ++j) {
-                if (fabs(c[i * block_len + j] - (double)N) > EPS) {
-                    printf(" Error! in ( %d , %d )-th argument in PE %d \n",j, i, myid);
+                if (fabs(c[i][j] - (double)N) > EPS) {
+                    printf(" Error! in ( %d , %d )-th argument in PE %d \n", i, j, myid);
                     iflag = 1;
                     ierr = MPI_Finalize();
                     goto END;
@@ -87,13 +103,10 @@ int main(int argc, char* argv[]) {
     }
 END:
     ierr = MPI_Finalize();
-    free(a);
-    free(b);
-    free(c);
     exit(0);
 }
 
-void MyMatMat(double* c, double* a, double* b, int n) {
+void MyMatMat(double* c, double* a, double* b) {
     int  i, j, k;
     int  ierr;
     int  isendPE, irecvPE;
@@ -102,34 +115,29 @@ void MyMatMat(double* c, double* a, double* b, int n) {
     /* Information of Send and recv PEs */
     isendPE = (myid + numprocs - 1) % numprocs;
     irecvPE = (myid + 1) % numprocs;
-    int jstart, block_len;
-    {
-        int istart = (n / numprocs) * myid;
-        int iend = MIN(istart + n / numprocs, n);
-        block_len = iend - istart;
-    }
-    double* buf = (double*)malloc(sizeof(b));
+    int jstart;
+    static double buf[N][BLOCK_LEN];
     for (process_i = 0; process_i < numprocs; ++process_i) {
-        jstart = block_len * ((process_i + myid) % numprocs);
-        for(i = 0; i < block_len; ++i) { // iは毎回同じ
-            for(j = 0; j < block_len; ++j) { // jは右にずれる
-	      //                c[i * block_len + j + jstart] = 0;
-                for (k = 0; k < n; ++k) {
-                    c[i * block_len + j] += a[i * block_len + k] * b[k * n + j]; 
+        jstart = BLOCK_LEN * ((process_i + myid) % numprocs);
+        for(i = 0; i < BLOCK_LEN; ++i) { // iは毎回同じ
+            for(j = 0; j < BLOCK_LEN; ++j) { // jは右にずれる
+                c[i][j + jstart] = 0;
+                for (k = 0; k < N; ++k) {
+                    c[i][j + jstart] += a[i][k] * b[k][j]; 
                 }
             }
         }
         if (process_i == numprocs) break;
         if ((myid & 1) == 0) { // 先に送信する
-            ierr = MPI_Send(b, n * block_len, MPI_DOUBLE, isendPE, myid + process_i, MPI_COMM_WORLD);
-            ierr = MPI_Recv(buf, n * block_len, MPI_DOUBLE, irecvPE, irecvPE + process_i, MPI_COMM_WORLD, &istatus);
+            ierr = MPI_Send(b, N * BLOCK_LEN, MPI_DOUBLE, isendPE, myid + process_i, MPI_COMM_WORLD);
+            ierr = MPI_Recv(buf, N * BLOCK_LEN, MPI_DOUBLE, irecvPE, irecvPE + process_i, MPI_COMM_WORLD, &istatus);
         } else {
-            ierr = MPI_Recv(buf, n * block_len, MPI_DOUBLE, irecvPE, irecvPE + process_i, MPI_COMM_WORLD, &istatus);
-            ierr = MPI_Send(b, n * block_len, MPI_DOUBLE, isendPE, myid + process_i, MPI_COMM_WORLD);
+            ierr = MPI_Recv(buf, N * BLOCK_LEN, MPI_DOUBLE, irecvPE, irecvPE + process_i, MPI_COMM_WORLD, &istatus);
+            ierr = MPI_Send(b, N * BLOCK_LEN, MPI_DOUBLE, isendPE, myid + process_i, MPI_COMM_WORLD);
         }
-        COPY(buf, b, n * block_len);
+        for (i = 0; i < N; ++i)
+            for (j = 0; j < BLOCK_LEN; ++j)
+                b[i][j] = buf[i][j];
     }
-    free(buf);
 }
-
 
