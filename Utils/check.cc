8b9f4271809bbe2acdabdabb308ee02d5f0a82a0
kngwyu <yuji.kngw.80s.revive@gmail.com>
2017-08-07 01:24:02 +0900

    modified checker

--- Utils/check.cc
+++ Utils/check.cc
@@ -6,25 +6,20 @@
 #include <map>
 using namespace std;
 int main() {
-    cout << "correct file: " << endl;
     map<pair<int, int>, double> mp;
     {
-        string s;
-        cin >> s;
-        ifstream ifs(s);
+        ifstream ifs("correct.dat");
         int i, j;
         double d;
         while (ifs >> i >> j >> d) 
             mp[make_pair(i, j)] = d;
     }
-    cout << "check file: " << endl;
-    string s;
-    cin >> s;
-    ifstream ifs(s);
+    ifstream ifs("check.dat");
     int i, j;
     double d;
     while (ifs >> i >> j >> d) {
-        cerr << i << ' ' << j << ' ' << d << ' ' << mp[make_pair(i, j)] << endl;
-        assert(fabs(mp[make_pair(i, j)] - d) < 1.0e-6);
+      cout << d << endl;
+      if(fabs(mp[make_pair(i, j)] - d) > 1.0e-6)
+	cout << i << ' ' << j << ' ' << d << ' ' << mp[make_pair(i, j)] << endl;
     }
 }
