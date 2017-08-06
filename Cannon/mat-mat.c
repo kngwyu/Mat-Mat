6dca27eae531bccd6579d32cb2e8d54d67e1504a
kngwyu <yuji.kngw.80s.revive@gmail.com>
2017-08-06 18:18:36 +0900

    implemented Cannon

--- Cannon/mat-mat.c
+++ Cannon/mat-mat.c
@@ -14,7 +14,7 @@
 #define  MAX(a, b) ((a) < (b) ? (b) : (a))
 
 int myid, numprocs;
-void MyMatMat(double c[BLOCK_LEN][N], double a[BLOCK_LEN][N], double b[N][BLOCK_
+void MyMatMat(double c[BLOCK_LEN][BLOCK_LEN], double a[BLOCK_LEN][BLOCK_LEN], double b[BLOCK_LEN][BLOCK_LEN]);
 int main(int argc, char* argv[]) {
     double  t0, t1, t2, t_w;
     double  dc_inv, d_mflops;
@@ -38,8 +38,6 @@ int main(int argc, char* argv[]) {
     ierr = MPI_Init(&argc, &argv);
     ierr = MPI_Comm_rank(MPI_COMM_WORLD, &myid);
     ierr = MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
-    // 自分が最初に持つ小行列の番号を決める
-    int my_i = myid / BLOCK_LEN, my_j = myid % BLOCK_LEN;
     static double a[BLOCK_LEN][BLOCK_LEN];
     static double b[BLOCK_LEN][BLOCK_LEN];
     static double c[BLOCK_LEN][BLOCK_LEN];
@@ -88,7 +86,7 @@ int main(int argc, char* argv[]) {
         /* Verification routine ----------------- */
         iflag = 0;
         for(i = 0; i < BLOCK_LEN; ++i) {
-            for (j = 0; j < N; ++j) {
+            for (j = 0; j < BLOCK_LEN; ++j) {
                 if (fabs(c[i][j] - (double)N) > EPS) {
                     printf(" Error! in ( %d , %d )-th argument in PE %d \n", i, j, myid);
                     iflag = 1;
@@ -109,38 +107,41 @@ END:
     exit(0);
 }
 
-void MyMatMat(double c[BLOCK_LEN][N], double a[BLOCK_LEN][N], double b[N][BLOCK_LEN]) {
-    int  i, j, k;
-    int  ierr;
-    int  isendPE, irecvPE;
-    int  process_i;
-    MPI_Status istatus;
-    /* Information of Send and recv PEs */
-    isendPE = (myid + numprocs - 1) % numprocs;
-    irecvPE = (myid + 1) % numprocs;
-    int jstart;
-    static double buf[N][BLOCK_LEN];
-    for (process_i = 0; process_i < numprocs; ++process_i) {
-        jstart = BLOCK_LEN * ((process_i + myid) % numprocs);
-        for(i = 0; i < BLOCK_LEN; ++i) { // iは毎回同じ
-            for(j = 0; j < BLOCK_LEN; ++j) { // jは右にずれる
-                c[i][j + jstart] = 0;
-                for (k = 0; k < N; ++k) {
-                    c[i][j + jstart] += a[i][k] * b[k][j]; 
-                }
-            }
-        }
-        if (process_i == numprocs) break;
-        if ((myid & 1) == 0) { // 先に送信する
-            ierr = MPI_Send(b, N * BLOCK_LEN, MPI_DOUBLE, isendPE, myid + process_i, MPI_COMM_WORLD);
-            ierr = MPI_Recv(buf, N * BLOCK_LEN, MPI_DOUBLE, irecvPE, irecvPE + process_i, MPI_COMM_WORLD, &istatus);
+void MyMatMat(double c[BLOCK_LEN][BLOCK_LEN], double a[BLOCK_LEN][BLOCK_LEN], double b[BLOCK_LEN][BLOCK_LEN]) {
+    // 一応二つ持つ
+    static double buf_a[BLOCK_LEN][BLOCK_LEN];
+    static double buf_b[BLOCK_LEN][BLOCK_LEN];
+    int i, j, k, ope;
+    int ierr;
+    // 自分が最初に持つ小行列の番号
+    int my_i = myid / BLOCK_LEN, my_j = myid % BLOCK_LEN;
+    // 左シフトするPE(縦の番号は同じ)
+    int left_pe = my_i * BLOCK_LEN + (my_j + BLOCK_LEN - 1) % BLOCK_LEN;
+    int right_pe = my_i * BLOCK_LEN + (my_j + 1) % BLOCK_LEN;
+    // 上シフトするPE(横の番号は同じ)
+    int up_pe = ((my_i + BLOCK_LEN - 1) % BLOCK_LEN) * BLOCK_LEN + my_j;
+    int down_pe = ((my_i + 1) % BLOCK_LEN) * BLOCK_LEN + my_j;
+    
+    for (ope = 0; ope < BLOCK_LEN; ++ope) {
+        for (i = 0; i < BLOCK_LEN; ++i) 
+            for (j = 0; j < BLOCK_LEN; ++j) 
+                for (k = 0; k < BLOCK_LEN; ++k)
+                    c[i][j] += a[i][k] + b[k][j];
+        // Aを左シフト
+        if ((my_i & 1) == 0) {  // 先に送信する
+             ierr = MPI_Send(a, BLOCK_LEN * BLOCK_LEN, MPI_DOUBLE, left_pe, myid * N + process_i, MPI_COMM_WORLD);
+             ierr = MPI_Recv(buf_a, BLOCK_LEN * BLOCK_LEN, MPI_DOUBLE, right_pe, right_pe * N + process_i, MPI_COMM_WORLD, &istatus);
         } else {
-            ierr = MPI_Recv(buf, N * BLOCK_LEN, MPI_DOUBLE, irecvPE, irecvPE + process_i, MPI_COMM_WORLD, &istatus);
-            ierr = MPI_Send(b, N * BLOCK_LEN, MPI_DOUBLE, isendPE, myid + process_i, MPI_COMM_WORLD);
+            ierr = MPI_Recv(buf_a, BLOCK_LEN * BLOCK_LEN, MPI_DOUBLE, right_pe, right_pe * N + process_i, MPI_COMM_WORLD, &istatus);
+            ierr = MPI_Send(a, BLOCK_LEN * BLOCK_LEN, MPI_DOUBLE, left_pe, myid * N + process_i, MPI_COMM_WORLD);
         }
-        for (i = 0; i < N; ++i)
-            for (j = 0; j < BLOCK_LEN; ++j)
-                b[i][j] = buf[i][j];
+        // Bを上シフト
+        if ((my_i & 1) == 0) {  // 先に送信する
+             ierr = MPI_Send(b, BLOCK_LEN * BLOCK_LEN, MPI_DOUBLE, up_pe, myid * N + process_i, MPI_COMM_WORLD);
+             ierr = MPI_Recv(buf_b, BLOCK_LEN * BLOCK_LEN, MPI_DOUBLE, down_pe, down_pe * N + process_i, MPI_COMM_WORLD, &istatus);
+        } else {
+            ierr = MPI_Recv(buf_b, BLOCK_LEN * BLOCK_LEN, MPI_DOUBLE, down_pe, down * N + process_i, MPI_COMM_WORLD, &istatus);
+            ierr = MPI_Send(b, BLOCK_LEN * BLOCK_LEN, MPI_DOUBLE, up_pe, myid * N + process_i, MPI_COMM_WORLD);
+        }
     }
 }
-
